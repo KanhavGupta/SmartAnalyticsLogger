6d01218f2cfa7740fa9a1c5ffaa8593922e9391c
KanhavGupta <33150698+KanhavGupta@users.noreply.github.com>
2025-07-04 14:26:28 +0530

    make AnalyticsCore internal

--- smartlogger/src/main/java/com/example/smartlogger/AnalyticsCore.kt
+++ smartlogger/src/main/java/com/example/smartlogger/AnalyticsCore.kt
@@ -2,7 +2,7 @@ package com.example.smartlogger
 
 import android.content.Context
 
-class AnalyticsCore(context: Context, uploadUrl: String) {
+internal class AnalyticsCore(context: Context, uploadUrl: String) {
     private val queue = EventQueueManager(context.applicationContext)
     private val uploader = EventUploader(context.applicationContext, uploadUrl, queue)
 
